486cd5868410328e24a5265401110343fc80c287
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2019-10-17 11:42:48 -0600

    Add files via upload

---
+++ paginas/Formulario.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="formulario.css">
+<body>
+
+<form action="ejemplo.php" method="get">
+<legend class="datos">Datos Usuario</legend>
+
+  <p class="name">Nombre:</p>
+  <p class="gmail">Correo Electronico:</p>
+  
+ <legend class="datos2">Datos Propuesta</legend>
+  <p class="prop">Ingrese Nombre propuesta: <input class="propuesta" type="text" name="propuesta"></p>
+  <p class="ods">ODS <input type="text" name="ods"></p>
+  <p class="des">Descripción<input type="text" name="descripcion"></p>
+  <p class="pro">Progreso<input type="text" name="progreso"></p>
+</form>
+
+</body>
+</html>
