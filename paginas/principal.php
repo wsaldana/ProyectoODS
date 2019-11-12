b71d20b8f85a0d92dd38ce4d4584dd337434a365
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2019-11-11 19:59:12 -0600

    Update principal.php

--- paginas/principal.php
+++ paginas/principal.php
@@ -6,11 +6,11 @@
 	</head>
 	<body style="background-image: url('imagenes/3.jpg');">
 	<table>
-		<td><a href="https://www.un.org/sustainabledevelopment/es/poverty/"><img src="https://i2.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-01.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=pobreza"><img src="https://i2.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-01.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 		
-		<td><a href="https://www.un.org/sustainabledevelopment/es/hunger/"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-02.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=hambre"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-02.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 		
-		<td><a href="https://www.un.org/sustainabledevelopment/es/health/"><img src="https://i1.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-03.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=salud"><img src="https://i1.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-03.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 	</tr>
 	<tr>
 	<td><p  style="color:white">Fin de la pobreza</p></td>
@@ -18,11 +18,11 @@
 	<td><p style="color:white">Salud y Bienestar</p></td>
 	</tr>
 	<tr>
-		<td><a href="https://www.un.org/sustainabledevelopment/es/education/"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-04.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=educacion"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-04.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 		
-		<td><a href="https://www.un.org/sustainabledevelopment/es/gender-equality/"><img src="https://i2.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-05.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=igualdad"><img src="https://i2.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-05.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 		
-		<td><a href="https://www.un.org/sustainabledevelopment/es/water-and-sanitation/"><img src="https://i2.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-06.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=saneamiento"><img src="https://i2.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-06.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 	</tr>
 	<tr>
 	<td><p style="color:white">Educacion de Calidad</p></td>
@@ -30,11 +30,11 @@
 	<td><p style="color:white">Agua limpia y saneamineto</p></td>
 	</tr>
 	<tr>
-		<td><a href=""><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-07.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=energias"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-07.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 		
-		<td><a href=""><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-08.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=crecimiento"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-08.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 		
-		<td><a href=""><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-09.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=infraestructura"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-09.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 	</tr>
 	<tr>
 	<td><p style="color:white">Energia Asequible y no contaminable</p></td>
@@ -42,11 +42,11 @@
 	<td><p style="color:white">Industria, innovacion e infraetructura</p></td>
 	</tr>
 	<tr>
-		<td><a href="https://www.un.org/sustainabledevelopment/es/inequality/"><img src="https://i1.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2015/09/S_SDG_Icons-01-10-1.jpg?fit=466%2C466&ssl=1';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=desiguadades"><img src="https://i1.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2015/09/S_SDG_Icons-01-10-1.jpg?fit=466%2C466&ssl=1';" height="200px" width="200px"/></a></td>
 		
-		<td><a href="https://www.un.org/sustainabledevelopment/es/cities/"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-11.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=asentamientos"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-11.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 		
-		<td><a href="https://www.un.org/sustainabledevelopment/es/sustainable-consumption-production/"><img src="https://i1.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-12.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=produccion"><img src="https://i1.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-12.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 	</tr>
 	<tr>
 	<td><p style="color:white">Reduccion de las desigualdades</p></td>
@@ -54,11 +54,11 @@
 	<td><p style="color:white">Produccion y consumo</p></td>
 	</tr>
 	<tr>
-		<td><a href="https://www.un.org/sustainabledevelopment/es/climate-change-2/"><img src="https://i1.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-13.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=clima"><img src="https://i1.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-13.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 		
-		<td><a href="https://www.un.org/sustainabledevelopment/es/oceans/"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-14.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=conservacion"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-14.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 		
-		<td><a href="https://www.un.org/sustainabledevelopment/es/biodiversity/"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-15.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=proteger"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-15.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 	</tr>
 	<tr>
 	<td><p style="color:white">Accion por el clima</p></td>
@@ -66,9 +66,9 @@
 	<td><p style="color:white">Vida de ecosistemas terrestres</p></td>
 	</tr>
 	<tr>
-		<td><a href="https://www.un.org/sustainabledevelopment/es/peace-justice/"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-16.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=promover"><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-16.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 		
-		<td><a href="https://www.un.org/sustainabledevelopment/es/globalpartnerships/"><img src="https://i1.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-17.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
+		<td><a href="paginas/Datos.php?ods=fortalecer"><img src="https://i1.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-17.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 		
 		<td><a href=""><img src="https://i0.wp.com/www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2015/09/S_SDG_Icons-01-18.jpg?zoom=1.25&w=1350';" height="200px" width="200px"/></a></td>
 	</tr>
